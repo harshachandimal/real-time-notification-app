ab7324a5e56fca11d98e3654b961e46fe377008f
Harsha <harshachandim92@gmail.com>
2025-08-27 22:07:23 +0530

    Installed laravel broadcasting, create event and config pusher from BE.

---
+++ app/Events/NotificationEvent.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class NotificationEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('real-time-app'),
+        ];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'notification.send';
+    }
+
+    public function broadcastWith(): array
+    {
+        return ['user_name' => 'Harsha'];
+    }
+}
